ec3356e0e98d260d2b704475ed8bca37be5579b8
Thomas Burgaud <thomas.burgaud@cashbee.fr>
2022-11-17 10:07:26 +0100

    Update posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 class PostsController < ApplicationController
   def index
-    posts = Post.all
-    @posts = posts.select do |content|
-      content.user&.email == params[:email]
-    end
+    @posts = Post.all
   end
 end
