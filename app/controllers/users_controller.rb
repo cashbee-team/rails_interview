516ccafbfe3335f661e68f222814d1af0e8eb37b
Thomas Burgaud <thomas.burgaud@cashbee.fr>
2022-11-17 11:04:07 +0100

    Update users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
+  def create
+    @user = User.create(params)
+  end
+
+  def update
+    @user = User.create(params)
+  end
+
   def index
     @users = User.all
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:user)
   end
 end
